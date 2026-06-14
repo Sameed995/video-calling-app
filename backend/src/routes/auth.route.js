43b849f6cbcb944da6abeb788851ed5838ac9030
Sameed995 <sameedshaikh346@gmail.com>
2026-06-14 13:04:40 +0530

    added a simple endpoint to check whether user is logged in or not

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,12 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);    
-router.post("/onboarding", protectRoute,onboard);                     
+router.post("/onboarding", protectRoute,onboard);      
+
+
+//check if user is logged in and return user data
+router.get("/me", protectRoute, (req, res) => {
+    res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
