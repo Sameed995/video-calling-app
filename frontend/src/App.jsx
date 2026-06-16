573893a5f55bc93fc559c06f9638cd4d97786a49
Sameed995 <sameedshaikh346@gmail.com>
2026-06-16 19:33:14 +0530

    initialised pages for authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+import SignupPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
 
 const App = () => {
   return (
-    <div className="bg-red-500 h-screen text-5xl">App</div>
+<div className=" h-screen" data-theme="dark">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/onboarding" element={<OnboardingPage />} />
+      <Route path="/notifications" element={<NotificationsPage />} />
+      <Route path="/call" element={<CallPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
+</div>
   )
 }
 
-export default App
+export default App;
