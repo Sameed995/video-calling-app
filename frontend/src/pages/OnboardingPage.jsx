8d6f5d4fa300e34a13301fd09cf799c17a4eff7f
Sameed995 <sameedshaikh346@gmail.com>
2026-06-23 19:10:40 +0530

    created a basic card UI

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { useState } from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { completeOnboarding } from "../lib/api";
+import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
 
 const OnboardingPage = () => {
+  const { authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const { mutate: onboardingMutation, isPending } = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile created successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  };
+  
   return (
-    <div>OnboardingPage</div>
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div className= "card bg-base-200 w-full max-w-3xl shadow-xl">
+        <div className="card-body p-6 sm:p-8">
+          <h1>Complete Your Profile</h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="flex flex-col items-center justify-center space-y-4">
+             <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile Preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )}
+                </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
