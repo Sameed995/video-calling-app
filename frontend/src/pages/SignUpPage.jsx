fdb76d14fddf09bd8a2f52dbec4a37ec388bee00
Sameed995 <sameedshaikh346@gmail.com>
2026-06-25 22:13:21 +0530

    added comments and optimised code

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import {useState} from 'react';
-import {MessageCircleMore} from "lucide-react";
-import { Link } from 'react-router';
-import { useMutation } from '@tanstack/react-query';
-import { useQueryClient } from '@tanstack/react-query';
-import { signup } from '../lib/api';
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+
+import useSignUp from "../hooks/useSignUp";
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
@@ -13,131 +11,143 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const queryClient = useQueryClient();
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: signupMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
 
-  const {mutate:signupMutation, isPending, error} = useMutation({
-    mutationFn: signup,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['authUser'] });
-    }
-  });
+  // This is how we did it using our custom hook - optimized version
+  const { isPending, error, signupMutation } = useSignUp();
 
   const handleSignup = (e) => {
     e.preventDefault();
     signupMutation(signupData);
-    // console.log(signupData);
-  }
-  console.log("ERROR OBJECT:", error);
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="light">
-       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-2xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
-       
-       {/* SignUp Form */}
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-3xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* SIGNUP FORM - LEFT SIDE */}
         <div className="w-full lg:w-1/1 p-4 sm:p-8 flex flex-col">
+          {/* LOGO */}
           <div className="mb-4 flex items-center justify-start gap-2">
-            <MessageCircleMore className="size-9 text-primary" />
+            <ShipWheelIcon className="size-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Learn New Languages
+              LinguaConnect
             </span>
           </div>
 
-          {/* error msg */}
-
+          {/* ERROR MESSAGE IF ANY */}
           {error && (
-            <div className="alert alert-error shadow-lg mb-4">
-              <span>
-                {error.response?.data?.message || "An error occurred during signup."}
-              </span>
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
             </div>
           )}
+
           <div className="w-full">
-            <form onSubmit={handleSignup} className="w-full flex flex-col gap-4">
-              <div className ="space-y-4">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
                 <div>
-                  <h2 className="text-xl font-semibold">Create Account</h2>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
                   <p className="text-sm opacity-70">
-                    Join this platform and start learning new languages with native speakers around the world.
+                    Join LinguaConnect and start your language learning adventure!
                   </p>
                 </div>
+
                 <div className="space-y-3">
+                  {/* FULLNAME */}
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Full Name</span>
                     </label>
                     <input
                       type="text"
-                      placeholder="Enter your full name"
+                      placeholder="John Doe"
                       className="input input-bordered w-full"
                       value={signupData.fullName}
-                      onChange={(e) => setSignupData({...signupData, fullName: e.target.value})}
+                      onChange={(e) => setSignupData({ ...signupData, fullName: e.target.value })}
                       required
                     />
                   </div>
+                  {/* EMAIL */}
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Email</span>
                     </label>
                     <input
                       type="email"
-                      placeholder="Enter your email"
+                      placeholder="john@gmail.com"
                       className="input input-bordered w-full"
                       value={signupData.email}
-                      onChange={(e) => setSignupData({...signupData, email: e.target.value})}
+                      onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
                       required
                     />
                   </div>
+                  {/* PASSWORD */}
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Password</span>
                     </label>
                     <input
                       type="password"
-                      placeholder="Enter your password"
+                      placeholder="********"
                       className="input input-bordered w-full"
                       value={signupData.password}
-                      onChange={(e) => setSignupData({...signupData, password: e.target.value})}
+                      onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
                       required
                     />
                     <p className="text-xs opacity-70 mt-1">
-                      Password must be at least 6 characters long and contain a mix of uppercase, lowercase, numbers, and special characters.
+                      Password must be at least 6 characters long
                     </p>
                   </div>
+
                   <div className="form-control">
-                    <label className="cursor-pointer label justify-start gap-2">
+                    <label className="label cursor-pointer justify-start gap-2">
                       <input type="checkbox" className="checkbox checkbox-sm" required />
                       <span className="text-xs leading-tight">
-                        I agree to the {""}
-                        <span className="text-primary hover:underline">Terms and Conditions</span> and {""}
-                        <span className="text-primary hover:underline">Privacy Policy</span>
+                        I agree to the{" "}
+                        <span className="text-primary hover:underline">terms of service</span> and{" "}
+                        <span className="text-primary hover:underline">privacy policy</span>
                       </span>
                     </label>
                   </div>
                 </div>
+
                 <button className="btn btn-primary w-full" type="submit">
-                  {isPending ?(<>
-                    <span className="loading loading-spinner loading-xs"></span>
-                    Loading...
-                  </>
-                ) : ("Create Account")
-                }
+                  {isPending ? (
+                    <>
+                      <span className="loading loading-spinner loading-xs"></span>
+                      Loading...
+                    </>
+                  ) : (
+                    "Create Account"
+                  )}
                 </button>
+
                 <div className="text-center mt-4">
                   <p className="text-sm">
                     Already have an account?{" "}
                     <Link to="/login" className="text-primary hover:underline">
-                      Log in
+                      Sign in
                     </Link>
                   </p>
                 </div>
-              </div>  
-            </form> 
-
+              </div>
+            </form>
           </div>
         </div>
-        </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignUpPage;
